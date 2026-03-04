7c1483b51ceec57ec1715ab3e1b4efb0475aebed
SamielEstrella <samielestrella123@gmail.com>
2026-03-04 12:04:59 -0500

    app_comit

--- Notebook/app.py
+++ Notebook/app.py
@@ -1,10 +1,6 @@
-import steamlit as st # Importación de Streamlit para crear la aplicación web
+import streamlit as st # Importación de Streamlit para crear la aplicación web
 import pandas as pd # Importación de pandas para manipulación de datos
-import plotly as plt # Importación de plotly para visualización de datos
-
-import pandas as pd
-import plotly.graph_objects as go  # Importación de plotly.graph_objects como go
-import streamlit as st
+import plotly as plt # Importación de plotly.graph_objects como go
 
 # Leer los datos del archivo CSV
 car_data = pd.read_csv('vehicles_us.csv')
