30f7265a867a23409c3516f2fe2fcaab979c9575
SamielEstrella <samielestrella123@gmail.com>
2026-03-04 11:16:38 -0500

    requirements

---
+++ Notebook/app.py
@@ -0,0 +1,4 @@
+import steamlit as st # Importación de Streamlit para crear la aplicación web
+import pandas as pd # Importación de pandas para manipulación de datos
+import plotly as plt # Importación de plotly para visualización de datos
+
