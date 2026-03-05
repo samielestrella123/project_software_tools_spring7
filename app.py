f48d5734b5fbcc3597daa8a5faff23f88c198fa6
SamielEstrella <samielestrella123@gmail.com>
2026-03-05 14:32:07 -0500

    go

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import plotly.graph_objects as plt  # Importación de plotly.graph_objects como go
+import plotly.graph_objects as go  # Importación de plotly.graph_objects como go
 import streamlit as st
 
 # Leer los datos del archivo CSV
@@ -15,7 +15,7 @@ if hist_button:
 
     # Crear un histograma utilizando plotly.graph_objects
     # Se crea una figura vacía y luego se añade un rastro de histograma
-    fig = plt.Figure(data=[plt.Histogram(x=car_data['odometer'])])
+    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
 
     # Opcional: Puedes añadir un título al gráfico si lo deseas
     fig.update_layout(title_text='Distribución del Odómetro')
